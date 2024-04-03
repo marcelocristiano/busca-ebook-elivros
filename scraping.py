ebb733a6c6760b148d0de036d6e9eb9d6da7b9f1
Marcelo Cristiano Alexandre - PrestServ <marcelo.cristiano.prestserv@petrobras.com.br>
2024-04-03 09:40:00 -0300

    Criação do scraping

--- scraping.py
+++ scraping.py
@@ -1,2 +1,12 @@
-def buscar_livro():
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+# Cria variavel para armazenar a URL do site a ser raspado
+url = 'http://lelivros.love/'
+
+# Executa a requisição ao site e armazena o retorno na variavel
+resp = requests.get(url)
+
+# Trasforma o objeto de resposta em html mais legivel
+soup = BeautifulSoup(resp.text, 'html.parser')
+print(soup)
