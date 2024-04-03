26bc4448f5e9a13aca5db4696956c810ba32323f
Marcelo Cristiano Alexandre - PrestServ <marcelo.cristiano.prestserv@petrobras.com.br>
2024-04-03 18:22:25 -0300

    Conclusão do scraping

--- app.py
+++ app.py
@@ -66,17 +66,49 @@ t_livro.heading('Download', text='Download')  # Define o cabeçalho da coluna 'D
 t_livro.bind("<Double-1>", abrir_link)
 
 # Insere dados de exemplo na tabela
-dados = [
-    ['Marcelo', 'Livro1', "https://www.google.com/"],
-    ['João', 'Livro2', "https://www.example.com/"],
-    ['Maria', 'Livro3', "https://www.python.org/"],
-    ['Ana', 'Livro4', "https://www.wikipedia.org/"],
-    ['Pedro', 'Livro5', "https://www.github.com/"],
-    ['Carlos', 'Livro6', "https://www.stackoverflow.com/"],
-    ['Juliana', 'Livro7', "https://www.openai.com/"],
-    ['Fernanda', 'Livro8', "https://www.microsoft.com/"],
-    ['Rafael', 'Livro9', "https://www.apple.com/"]
+dados = livros = [
+    ['Auguste Comte', 'Curso de Filosofia Positiva, Discurso Sobre o Espírito Positivo', 'https://elivros.love/livro/baixar-livro-curso-de-filosofia-positiva-discurso-sobre-o-espirito-positivo-auguste-comte-em-epub-pdf-mobi-ou-ler-online'],
+    ['Augusto Cury', 'Ansiedade - Como Enfrentar o Mal do Século', 'https://elivros.love/livro/baixar-livro-ansiedade-como-enfrentar-o-mal-do-seculo-augusto-cury-em-epub-pdf-mobi-ou-ler-online'],
+    ['Augusto Cury', 'Ansiedade 3: Ciúme', 'https://elivros.love/livro/baixar-livro-ansiedade-3-ciume-augusto-cury-em-epub-pdf-mobi-ou-ler-online'],
+    ['Augusto Cury', 'Armadilhas da Mente', 'https://elivros.love/livro/baixar-livro-armadilhas-da-mente-augusto-cury-em-epub-pdf-mobi-ou-ler-online'],
+    ['Augusto Cury', 'A Fascinante Construção do Eu', 'https://elivros.love/livro/baixar-livro-fascinante-construcao-do-eu-augusto-cury-em-epub-pdf-mobi-ou-ler-online'],
+    ['Augusto Cury', 'Em Busca do Sentido da Vida', 'https://elivros.love/livro/baixar-livro-em-busca-do-sentido-da-vida-augusto-cury-epub-pdf-mobi-ou-ler-online'],
+    ['Augusto Cury', 'Nunca Desista dos Seus Sonhos', 'https://elivros.love/livro/baixar-livro-nunca-desista-dos-seus-sonhos-augusto-cury-em-epub-pdf-mobi-ou-ler-online'],
+    ['Augusto Cury', 'O Colecionador de Lágrimas', 'https://elivros.love/livro/baixar-livro-o-colecionador-de-lágrimas-augusto-cury-em-epub-pdf-mobi-ou-ler-online'],
+    ['Carlos Augusto de Proença Rosa', 'História da Ciência: Da Antiguidade ao Renascimento Científico', 'https://elivros.love/livro/baixar-livro-historia-da-ciencia-da-antiguidade-ao-renascimento-cientifico-carlos-augusto-de-proenca-rosa-em-epub-pdf-mobi-ou-ler-online'],
+    ['Carlos Augusto Lopes Filho', 'Os Desbravadores', 'https://elivros.love/livro/baixar-livro-os-desbravadores-carlos-augusto-lopes-filho-em-epub-pdf-mobi-ou-ler-online'],
+    ['Cissa Guimarães', 'Viver Com Fé - Histórias de Quem Acredita', 'https://elivros.love/livro/baixar-viver-com-fe-historias-de-quem-acredita-cissa-guimaraes-epub-pdf-mobi-ou-ler-online'],
+    ['Daniel Coyle', 'Equipes Brilhantes: Como Criar Grupos Fortes e Motivados', 'https://elivros.love/livro/baixar-livro-equipes-brilhantes-como-criar-grupos-fortes-e-motivados-daniel-coyle-em-epub-pdf-mobi-ou-ler-online'],
+    ['Denise Assis', 'Claudio Guerra: Matar e Queimar', 'https://elivros.love/livro/baixar-livro-claudio-guerra-matar-queimar-denise-assis-em-epub-pdf-mobi-ou-ler-online'],
+    ['Flávio Augusto da Silva', 'Ponto de Inflexão', 'https://elivros.love/livro/baixar-livro-ponto-de-inflexao-flavio-augusto-da-silva-em-epub-pdf-mobi-ou-ler-online'],
+    ['George Orwell', 'O que é Fascismo? e Outros Ensaios', 'https://elivros.love/livro/baixar-livro-o-que-e-fascismo-e-outros-ensaios-george-orwell-em-epub-pdf-mobi-ou-ler-online'],
+    ['Henrique Schneider', 'A Solidão do Amanhã', 'https://elivros.love/livro/baixar-livro-solidao-do-amanha-henrique-schneider-em-epub-pdf-mobi-ou-ler-online'],
+    ['Hermann Broch', 'A Morte de Virgílio', 'https://elivros.love/livro/baixar-livro-morte-de-virgilio-hermann-broch-em-epub-pdf-mobi-ou-ler-online'],
+    ['Ivan Bichara', 'Carcará', 'https://elivros.love/livro/baixar-livro-carcara-ivan-bichara-em-epub-pdf-mobi-ou-ler-online'],
+    ['Jas Silva', 'O Barão do Café', 'https://elivros.love/livro/baixar-livro-o-barao-do-cafe-jas-silva-em-epub-pdf-mobi-ou-ler-online'],
+    ['João Guimarães Rosa', 'A Hora e Vez de Augusto Matraga', 'https://elivros.love/livro/baixar-livro-a-hora-e-vez-de-augusto-matraga-joao-guimaraes-rosa-em-epub-pdf-mobi-ou-ler-online'],
+    ['João Guimarães Rosa', 'Sagarana', 'https://elivros.love/livro/baixar-livro-sagarana-joao-guimaraes-rosa-em-epub-pdf-mobi-ou-ler-online'],
+    ['Joaquim Campelo Marques', 'História da Literatura Ocidental', 'https://elivros.love/livro/baixar-livro-historia-da-literatura-ocidental-joaquim-campelo-marques-em-epub-pdf-mobi-ou-ler-online'],
+    ['Jose Augusto Minarelli', 'Superdicas de Networking para sua vida Pessoal e Profissional', 'https://elivros.love/livro/baixar-livro-superdicas-de-networking-para-sua-vida-pessoal-profissional-jose-augusto-minarelli-em-epub-pdf-mobi-ou-ler-online'],
+    ['Luís Augusto Fischer', 'Dicionário de Porto-Alegrês', 'https://elivros.love/livro/baixar-livro-dicionario-de-porto-alegres-luis-augusto-fischer-em-epub-pdf-mobi-ou-ler-online'],
+    ['Luiz Alberto Moniz Bandeira', 'Fórmula para o Caos', 'https://elivros.love/livro/baixar-livro-formula-para-caos-luiz-alberto-moniz-bandeira-em-epub-pdf-mobi-ou-ler-online'],
+    ['Marco Lucchesi', 'O Bibliotecário do Imperador', 'https://elivros.love/livro/baixar-o-bibliotecario-do-imperador-marco-lucchesi-epub-pdf-mobi-ou-ler-online'],
+    ['Mary Del Priore', 'O Príncipe Maldito', 'https://elivros.love/livro/baixar-o-principe-maldito-mary-del-priore-epub-pdf-mobi-ou-ler-online'],
+    ['Nísia Floresta', 'Fany ou o Modelo das Donzelas', 'https://elivros.love/livro/baixar-livro-fany-ou-modelo-das-donzelas-nisia-floresta-em-epub-pdf-mobi-ou-ler-online'],
+    ['Olivier Besancenot', 'Setembro Vermelho: O Golpe de Estado no Chile em 1973', 'https://elivros.love/livro/baixar-livro-setembro-vermelho-golpe-de-estado-no-chile-em-1973-olivier-besancenot-em-epub-pdf-mobi-ou-ler-online'],
+    ['Ricardo Seitenfus', 'Manual das Organizações Internacionais', 'https://elivros.love/livro/baixar-livro-manual-das-organizacoes-internacionais-ricardo-seitenfus-em-epub-pdf-mobi-ou-ler-online'],
+    ['Rogerio Augusto Schmitt', 'Partidos Políticos No Brasil (1945-2000)', 'https://elivros.love/livro/baixar-livro-partidos-politicos-no-brasil-1945-2000-rogerio-augusto-schmitt-em-epub-pdf-mobi-ou-ler-online'],
+    ['Rubem Fonseca', 'Mandrake: A Bíblia e a Bengala', 'https://elivros.love/livro/baixar-livro-mandrake-biblia-bengala-rubem-fonseca-em-epub-pdf-mobi-ou-ler-online'],
+    ['Rubem Fonseca', 'Os Prisioneiros Rubem Fonseca', 'https://elivros.love/livro/baixar-os-prisioneiros-rubem-fonseca-rubem-fonseca-epub-pdf-mobi-ou-ler-online'],
+    ['R. Augusto', 'Imunologia Fundamental: Medbook', 'https://elivros.love/livro/baixar-livro-imunologia-fundamental-medbook-r-augusto-em-epub-pdf-mobi-ou-ler-online'],
+    ['Roosevelt Augusto', 'O Fraseador', 'https://elivros.love/livro/baixar-livro-o-fraseador-roosevelt-augusto-em-epub-pdf-mobi-ou-ler-online'],
+    ['Sérgio Sant’Anna', 'Páginas Sem Glória', 'https://elivros.love/livro/baixar-paginas-sem-gloria-sergio-santanna-epub-pdf-mobi-ou-ler-online'],
+    ['Sergio Augusto de Avellar Coutinho', 'A Revolução Gramscista No Ocidente', 'https://elivros.love/livro/baixar-livro-a-revolucao-gramscista-no-ocidente-sergio-augusto-de-avellar-coutinho-em-epub-pdf-mobi-ou-ler-online'],
+    ['Vinicius de Moraes', '50 Poemas Macabros', 'https://elivros.love/livro/baixar-livro-50-poemas-macabros-vinicius-de-moraes-em-epub-pdf-mobi-ou-ler-online'],
+    ['William Shakespeare', 'Antônio e Cleópatra', 'https://elivros.love/livro/baixar-antonio-e-cleopatra-william-shakespeare-epub-pdf-mobi-ou-ler-online'],
+    ['Conn Iggulden', 'Sangue dos Deuses', 'https://elivros.love/livro/baixar-livro-sangue-dos-deuses-o-imperador-vol-5-conn-iggulden-em-epub-pdf-mobi-ou-ler-online']
 ]
+
 
 # Adiciona uma ligação de clique à tag "hyperlink"
 t_livro.tag_bind("hyperlink", "<Button-1>", abrir_link)
--- scraping.py
+++ scraping.py
@@ -2,11 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 # Cria variavel para armazenar a URL do site a ser raspado
-url = 'http://lelivros.love/'
+url = 'https://elivros.love/Search/augusto-cury'
+def pesquisar(url):
+    # Executa a requisição ao site e armazena o retorno na variavel
+    resp = requests.get(url)
+    # Trasforma o objeto de resposta em html mais legivel
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    return soup
 
-# Executa a requisição ao site e armazena o retorno na variavel
-resp = requests.get(url)
 
-# Trasforma o objeto de resposta em html mais legivel
-soup = BeautifulSoup(resp.text, 'html.parser')
-print(soup)
+def buscar_paginas(soup):
+    links_paginas = []
+    paginas = soup.find_all(class_='page-link')
+    for pagina in paginas[1:]:
+        links_paginas.append(pagina['href'].strip())
+    return links_paginas
+
+def buscar_livro(links_paginas: list):
+    livros = []
+    for link_pagina in links_paginas:
+        # Executa a requisição ao site e armazena o retorno na variavel
+        resp = requests.get(link_pagina)
+
+        # Trasforma o objeto de resposta em html mais legivel
+        soup_pagina = BeautifulSoup(resp.text, 'html.parser')
+
+        class_livros = soup_pagina.find_all(class_="bInfo")
+
+        for info_livro in class_livros:
+            autor = info_livro.find(class_="autor").text.strip()
+            titulo = info_livro.find('a').text.strip()
+            link = 'https:' + info_livro.find('a')['href']
+            livros.append([autor, titulo, link])
+    
+        #livros = sorted(livros, key=lambda x: x[0])
+    return livros
